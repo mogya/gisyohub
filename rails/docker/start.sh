4b448050ce58685127c6c4419ad057cdb0c658c8
daisuke furukawa <mogya@mogya.com>
2019-10-06 08:37:21 +0900

    set user for docker on linux.

--- rails/docker/start.sh
+++ rails/docker/start.sh
@@ -3,18 +3,18 @@ mkdir -p /log/unicorn/
 mkdir -p /log/nginx/
 mkdir -p /db/postgres/data
 mkdir -p /db/mysql/data
-pwd
-ls -la
-echo 'rake db:migrate'
-bundle exec rake db:migrate
 echo "RAILS_ENV:$RAILS_ENV"
 if [ "$RAILS_ENV" = "development" ]
 then
   echo 'just start container and wait connection.'
+  pwd
+  ls -la
   tail -f /dev/null
 else
+  echo 'rake db:migrate'
+  bin/rake db:migrate
   echo 'rake assets:precompile...'
-  bundle exec rake assets:precompile
+  bin/rake assets:precompile
   echo 'start unicorn server.'
   rm /tmp/unicorn.pid
   bundle exec unicorn -c /rails/docker/unicorn.rb
