60b085a3a6c5ae6ac84e1dc618e9e3d925ff507a
daisuke furukawa <mogya@mogya.com>
2020-02-14 13:31:16 +0900

    keep redirect url on cokkie for after log-in.

--- front/middleware/must_be_worker.js
+++ front/middleware/must_be_worker.js
@@ -1,12 +1,15 @@
-export default function ({ app, redirect }) {
+export default function ({ app, route, redirect }) {
   if (app.$auth) {
     if (app.$auth.hasScope('worker') || app.$auth.hasScope('administrator')) {
+      // Okay, go.
     } else {
       console.log('user is logged-in but no privillage')
-      return redirect(app.$front_url('/sessions/new'))
+      app.$cookies.set('auth.redirect', route.fullPath)
+      return redirect('/sessions/new')
     }
   } else {
     console.log('user is not logged-in')
-    return redirect(app.$front_url('/sessions/new'))
+    app.$cookies.set('auth.redirect', route.fullPath)
+    return redirect('/sessions/new')
   }
 }
