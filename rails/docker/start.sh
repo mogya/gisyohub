bdc0ef32ac55c6d3a89bf632bb47680f7d5351eb
mogya <mogya@mogya.com>
2020-02-25 13:20:59 +0900

    heroku対応

--- rails/docker/start.sh
+++ rails/docker/start.sh
@@ -1,13 +1,5 @@
 echo "RAILS_ENV:$RAILS_ENV"
-if [ "$RAILS_ENV" = "development" ]
-then
-  # pwd
-  # ls -la
-  bin/setup
-  /bin/bash && tail -f /dev/null
-else
-  bin/setup
-  echo 'start unicorn server.'
-  rm -f /tmp/unicorn.pid
-  bundle exec unicorn -c /rails/docker/unicorn.rb
-fi
+echo "PORT:$PORT"
+rm -f tmp/pids/server.pid
+bin/rails s -p $PORT -b 0.0.0.0 &
+tail -f /dev/null
