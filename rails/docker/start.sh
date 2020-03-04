7d363dfc8e0a5c1d1ebdf9a66d4c6e6c0926b822
daisuke furukawa <mogya@mogya.com>
2020-03-04 15:10:07 +0900

    fix issue to fail bundle install on develop env

--- rails/docker/start.sh
+++ rails/docker/start.sh
@@ -1,5 +1,23 @@
 echo "RAILS_ENV:$RAILS_ENV"
 echo "PORT:$PORT"
+# pwd
+# ls -la
 rm -f tmp/pids/server.pid
-bin/rails s -p $PORT -b 0.0.0.0 &
+
+if [ "$RAILS_ENV" = "development" ]
+then
+  echo 'gem: --no-rdoc --no-ri' >> "$HOME/.gemrc"
+  export GEM_HOME=/rails/vendor/bundle
+  export PATH=$GEM_HOME/bin:$PATH
+  bundle config --local path "$GEM_HOME"
+  bundle config --local bin "$GEM_HOME/bin"
+  export BUNDLE_APP_CONFIG=$GEM_HOME
+  gem install bundler
+  bundle install -j4 --retry=3
+  bin/setup
+  bin/rails db:seed
+  bin/rails s -p $PORT -b 0.0.0.0 &
+else
+  bin/rails s -p $PORT -b 0.0.0.0 &
+fi
 tail -f /dev/null
