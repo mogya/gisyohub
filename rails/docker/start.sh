908b5228629ecb38556ec8d7a37cf8b5dc0fc7dc
FURUKAWA,Daisuke <mogya@mogya.com>
2020-03-05 15:44:41 +0900

    use Heroku for rails container (#11)
    * use postgresql on development env.
    
    * update ci rspec db enviroment.
    
    * fix issue to fail bundle install on develop env
    
    * github action to release on Heroku
    
    * stop nginx container for temporarily

--- rails/docker/start.sh
+++ rails/docker/start.sh
@@ -1,13 +1,23 @@
 echo "RAILS_ENV:$RAILS_ENV"
+echo "PORT:$PORT"
+# pwd
+# ls -la
+rm -f tmp/pids/server.pid
+
 if [ "$RAILS_ENV" = "development" ]
 then
-  # pwd
-  # ls -la
+  echo 'gem: --no-rdoc --no-ri' >> "$HOME/.gemrc"
+  export GEM_HOME=/rails/vendor/bundle
+  export PATH=$GEM_HOME/bin:$PATH
+  bundle config --local path "$GEM_HOME"
+  bundle config --local bin "$GEM_HOME/bin"
+  export BUNDLE_APP_CONFIG=$GEM_HOME
+  gem install bundler
+  bundle install -j4 --retry=3
   bin/setup
-  /bin/bash && tail -f /dev/null
+  bin/rails db:seed
+  bin/rails s -p $PORT -b 0.0.0.0 &
 else
-  bin/setup
-  echo 'start unicorn server.'
-  rm -f /tmp/unicorn.pid
-  bundle exec unicorn -c /rails/docker/unicorn.rb
+  bin/rails s -p $PORT -b 0.0.0.0 &
 fi
+tail -f /dev/null
