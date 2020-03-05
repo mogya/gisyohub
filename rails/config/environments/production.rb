64ec07e2bc0261d5c914c2bf296863711f611bae
daisuke furukawa <mogya@mogya.com>
2020-03-05 16:42:32 +0900

    allow heroku domain

--- rails/config/environments/production.rb
+++ rails/config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << '.gisyohub.com'
   config.hosts << 'localhost'
+  config.hosts << 'gisyohub.herokuapp.com'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
